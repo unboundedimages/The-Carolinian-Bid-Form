d0e52cb6e8d04994f4e38a57b97b10839cc16844
unboundedimages <majid.muhassan@gmail.com>
2018-08-07 22:19:27 -0400

    added bold and italic button, with a possible cdn to handle manipulation of bold and italic text.  Current issue with the cdn is that you can not tab out of it, and the focus doesn't work with it.

---
+++ app.js
@@ -0,0 +1,16 @@
+function bold(){
+	document.getElementbyId("textarea1").style.fontWeight = 'bold';
+}
+
+// if(this.value == 'u')
+// textarea.style.textDecoration = 'underline';
+// else
+// textarea.style.textDecoration = '';
+// if(this.value == 'i')
+// textarea.style.fontStyle = 'italic';
+// else
+// textarea.style.fontStyle = 'normal';
+// if(this.value == 'b')
+// textarea.style.fontWeight = 'bold';
+// else
+// textarea.style.fontWeight = '';
