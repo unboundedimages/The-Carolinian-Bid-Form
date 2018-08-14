b9395b6cd59949fd0b9b6d62cd0fba86a528bd7f
unboundedimages <majid.muhassan@gmail.com>
2018-08-14 14:51:09 -0400

    fixed issue with the newsman wasn't showing up in MS Edge.  Also fixed issue when clicking into text box, the text now goes to begining of the containter.  Fixed issue where justify wasn't working in MS Edge, and iOS due to quill.js framework.

--- app.js
+++ app.js
@@ -27,3 +27,13 @@ function bold() {
 // textarea.style.fontWeight = 'bold';
 // else
 // textarea.style.fontWeight = '';
+
+//add place holder for the text area
+
+// Let it say Headline 1 
+// then headline 2 for the sub headlineand then
+// body text so when they start typing in the area
+// it is already formated to the necessary style
+
+// Need helvitica 65 Neue for the body 
+// helvetica -85 heavy for the headline 
