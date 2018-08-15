83dc795a3b0e84264490b372329be28300953a21
unboundedimages <majid.muhassan@gmail.com>
2018-08-15 13:26:31 -0400

    fixed issue with too many numbers showing to the right of the decimal.  It now shows just two numbers.

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-// function bold() {
-// 	var norm = document.getElementById("textarea1");
-// 	if (norm.style.fontWeight == 'bold') {
-// 		norm.style.fontWeight = "normal"
-// 		// alert("alert")
-// 	} 
-// 	else {
-// 		norm.style.fontWeight = "bold"
-// // alert("alert111111111111111")
-// }
-
-// }
-
-
-// function money(event) {
-// 	var key = event.keyCode;
-// 	var forumla = key == 13;
-// 	var x = 1.56
-// 	if (forumla ){
-// 		// alert("alert111111111111111")
-// 		document.getElementById('estimate').value = parseInt(document.getElementById('estimate').value) + 2;
-// 	}
-// }
+//Use an event listener to 
 document.addEventListener('keyup', checkKey);
 
 function checkKey(event) {
@@ -31,51 +9,11 @@ function checkKey(event) {
 }
 
 //make a variable that ++ each time enter is pressed. And thta will be muliplied by 1.56
-
+// Ensure that the decimal prints out the 100th place, 2 places to the right of the deciimal only
 var x = 2;
-var y = 1.56;
 function money() {
-	// var y = Math.floor(Math.random() * 13);
-	// var z = x * y;
-	// var zz= x + x;
-	return document.getElementById('estimate').innerHTML = "$"+1.56 * x++;
+	return document.getElementById('estimate').innerHTML = "$"+ (1.56 * x++).toFixed(2);
 }
 
 
-// function money(event) {
-// 	var key = event.keyCode;
-// 	var forumla = key == 13;
-// 	var x = 1.56
-// 	if (forumla ){
-// 		// alert("alert111111111111111")
-// 		document.getElementById('estimate').innerHTML = x + x;
-// 	}
-// }
-
-
-// function bold(){
-// 	document.getElementbyId("textarea1").style.fontWeight = 'bold';
-// }
-
-// if(this.value == 'u')
-// textarea.style.textDecoration = 'underline';
-// else
-// textarea.style.textDecoration = '';
-// if(this.value == 'i')
-// textarea.style.fontStyle = 'italic';
-// else
-// textarea.style.fontStyle = 'normal';
-// if(this.value == 'b')
-// textarea.style.fontWeight = 'bold';
-// else
-// textarea.style.fontWeight = '';
-
-//add place holder for the text area
-
-// Let it say Headline 1 
-// then headline 2 for the sub headlineand then
-// body text so when they start typing in the area
-// it is already formated to the necessary style
-
-// Need helvitica 65 Neue for the body 
-// helvetica -85 heavy for the headline 
+//create a formula that counts the lines and multiplies it by 1.56
