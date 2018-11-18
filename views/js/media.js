b4e871428b3b29fd745155520cd90f364e336998
unboundedimages <majid.muhassan@gmail.com>
2018-11-18 01:22:03 -0500

    moved lines 15-22 to app.js

--- views/js/media.js
+++ views/js/media.js
@@ -12,11 +12,11 @@ function copyRecord(e) {
 	document.execCommand("copy");
 	// alert("Copied the text: " + x.value);
 }
-//strips formatting when text is pasted into text area
-document.querySelector("div[contenteditable]").addEventListener("paste", function(e) {
-    e.preventDefault();
-    var text = e.clipboardData.getData("text/plain");
-    var temp = document.createElement("div");
-    temp.innerHTML = text;
-    document.execCommand("insertHTML", false, temp.textContent);
-});
+// //strips formatting when text is pasted into text area
+// document.querySelector("div[contenteditable]").addEventListener("paste", function(e) {
+//     e.preventDefault();
+//     var text = e.clipboardData.getData("text/plain");
+//     var temp = document.createElement("div");
+//     temp.innerHTML = text;
+//     document.execCommand("insertHTML", false, temp.textContent);
+// });
