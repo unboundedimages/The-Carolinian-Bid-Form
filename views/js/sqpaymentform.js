9900938936324d0ebd47d0072c0fd423621fb7e2
unboundedimages <majid.muhassan@gmail.com>
2018-11-23 04:54:16 -0500

    updated the css for the toast on the index.hbs and ccpytgm.hbs

--- views/js/sqpaymentform.js
+++ views/js/sqpaymentform.js
@@ -1,10 +1,9 @@
-// Set the application ID
-var applicationId = "sandbox id";
-// var applicationId = "production id";
+// var applicationId = "sandbox--";
+var applicationId = "-";
 
 // Set the location ID
-var locationId = "add sandbox locatioin";
-// var locationId = "production location";
+// var locationId = "";
+var locationId = "";
 
 /*
  * function: requestCardNonce
@@ -29,7 +28,9 @@ var paymentForm = new SqPaymentForm({
 
   // Initialize the payment form elements
   applicationId: applicationId,
+  // applicationId: keys.getArr(),
   locationId: locationId,
+  // locationId: keys.getArr(),
   inputClass: 'sq-input',
 
   // Customize the CSS for SqPaymentForm iframe elements
@@ -38,19 +39,19 @@ var paymentForm = new SqPaymentForm({
   }],
 
   // Initialize Apple Pay placeholder ID
-  applePay: {
-    elementId: 'sq-apple-pay'
-  },
+  // applePay: {
+  //   elementId: 'sq-apple-pay'
+  // },
 
   // Initialize Google Pay placeholder ID
-  googlePay: {
-    elementId: 'sq-google-pay'
-  },
+  // googlePay: {
+  //   elementId: 'sq-google-pay'
+  // },
 
   // Initialize Masterpass placeholder ID
-  masterpass: {
-    elementId: 'sq-masterpass'
-  },
+  // masterpass: {
+  //   elementId: 'sq-masterpass'
+  // },
 
   // Initialize the credit card placeholders
   cardNumber: {
@@ -81,29 +82,29 @@ var paymentForm = new SqPaymentForm({
      * is enabled or not. For example, if Google Pay is available, `methods`
      * will be the object: {googlePay: true}.
      */
-    methodsSupported: function (methods) {
+    // methodsSupported: function (methods) {
 
-      var applePayBtn = document.getElementById('sq-apple-pay');
-      var googlePayBtn = document.getElementById('sq-google-pay');
-      var masterpassBtn = document.getElementById('sq-masterpass');
+    //   var applePayBtn = document.getElementById('sq-apple-pay');
+    //   var googlePayBtn = document.getElementById('sq-google-pay');
+    //   var masterpassBtn = document.getElementById('sq-masterpass');
 
-      // Only show the button if Apple Pay on the Web is enabled
-      // Otherwise, display the wallet not enabled message.
-      if (methods.applePay === true) {
-        applePayBtn.style.display = 'inline-block';
-      }
+    //   // Only show the button if Apple Pay on the Web is enabled
+    //   // Otherwise, display the wallet not enabled message.
+    //   if (methods.applePay === true) {
+    //     applePayBtn.style.display = 'inline-block';
+    //   }
 
-      // Only show the button if Google Pay is enabled
-      if (methods.googlePay === true) {
-        googlePayBtn.style.display = 'inline-block';
-      }
+    //   // Only show the button if Google Pay is enabled
+    //   if (methods.googlePay === true) {
+    //     googlePayBtn.style.display = 'inline-block';
+    //   }
 
-      // Only show the button if Masterpass is enabled
-      // Otherwise, display the wallet not enabled message.
-      if (methods.masterpass === true) {
-        masterpassBtn.style.display = 'inline-block';
-      }
-    },
+    //   // Only show the button if Masterpass is enabled
+    //   // Otherwise, display the wallet not enabled message.
+    //   if (methods.masterpass === true) {
+    //     masterpassBtn.style.display = 'inline-block';
+    //   }
+    // },
 
     /*
      * callback function: createPaymentRequest
@@ -158,8 +159,39 @@ var paymentForm = new SqPaymentForm({
      * callback function: cardNonceResponseReceived
      * Triggered when: SqPaymentForm completes a card nonce request
      */
+    
     cardNonceResponseReceived: function(errors, nonce, cardData, billingContact, shippingContact) {
+      function launch_toast2() {
+        var x = document.getElementById("toast2")
+        x.className = "show";
+        x.hidden = false;
+        setTimeout(function(){ 
+          x.className = x.className.replace("show", ""); 
+          x.hidden = true;
+        }, 5000);
+    }
+    function shake(){
+      var sha= {
+        cardnumber: document.getElementById("sq-card-number"),
+        cvv: document.getElementById("sq-cvv"),
+        expiration: document.getElementById("sq-expiration-date"),
+        zipcode: document.getElementById("sq-postal-code"),
+
+      }
+        sha.cardnumber.classList.add('shake-in');
+        sha.cvv.classList.add('shake-in');
+        sha.expiration.classList.add('shake-in');
+        sha.zipcode.classList.add('shake-in');
+         setTimeout(function() {
+        sha.cardnumber.classList.remove('shake-in');   
+        sha.cvv.classList.remove('shake-in'); // Remove .fade-in class
+        sha.expiration.classList.remove('shake-in');
+        sha.zipcode.classList.remove('shake-in');
+      }, 1000);
+    } 
       if (errors) {
+        launch_toast2()
+        shake()
         // Log errors from nonce generation to the Javascript console
         console.log("Encountered errors:");
         errors.forEach(function(error) {
@@ -168,8 +200,6 @@ var paymentForm = new SqPaymentForm({
 
         return;
       }
-
-      alert('Nonce received: ' + nonce); /* FOR TESTING ONLY */
 
       // Assign the nonce value to the hidden form field
       document.getElementById('card-nonce').value = nonce;
