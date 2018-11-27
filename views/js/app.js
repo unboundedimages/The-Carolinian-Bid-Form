2782c4556870aecb72caf4f98ebcc4a244bcb67a
unboundedimages <majid.muhassan@gmail.com>
2018-11-27 12:26:37 -0500

    added function to aid the insertOrderedList command.  The function corrects the issue where the document.execCommand did not work well the unordered list.

--- views/js/app.js
+++ views/js/app.js
@@ -156,8 +156,6 @@ function show_inputs(that) {
 
 //greys out dates prior to today.
 var today = new Date().toISOString().split('T')[0];
-//     // document.getElementsByName("date")[0].setAttribute('min', today);
-    // document.getElementsByClassName('date')[0].setAttribute('min', today);
     document.getElementsByName('input1')[0].setAttribute('min', today);
     document.getElementsByName('input2')[0].setAttribute('min', today);
     document.getElementsByName('input3')[0].setAttribute('min', today);
@@ -227,3 +225,57 @@ document.querySelector("div[contenteditable]").addEventListener("paste", functio
     temp.innerHTML = text;
     document.execCommand("insertHTML", false, temp.textContent);
 });
+
+function execCommandOnElement(el, commandName, value) {
+    
+    if (typeof value == "undefined") {
+        value = null;
+    }
+
+    if (typeof window.getSelection != "undefined") {
+        // Non-IE case
+        var sel = window.getSelection();
+
+        // Save the current selection
+        var savedRanges = [];
+        for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+            savedRanges[i] = sel.getRangeAt(i).cloneRange();
+        }
+
+        // Temporarily enable designMode so that
+        // document.execCommand() will work
+        document.designMode = "on";
+
+        // Select the element's content
+        sel = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+
+        // Execute the command
+        document.execCommand(commandName, false, value);
+
+        // Disable designMode
+        document.designMode = "off";
+
+        // Restore the previous selection
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        for (var i = 0, len = savedRanges.length; i < len; ++i) {
+            sel.addRange(savedRanges[i]);
+        }
+    } else if (typeof document.body.createTextRange != "undefined") {
+        // IE case
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.execCommand(commandName, false, value);
+    }
+}
+
+function removeDot(){
+    execCommandOnElement(document.getElementById("ta"), "insertUnorderedlist");
+    document.getElementById("ta").innerHTML="";
+}
+removeDot()
+// execCommandOnElement(document.getElementById("ta"), "insertUnorderedlist");
