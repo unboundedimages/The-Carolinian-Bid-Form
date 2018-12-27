47714554c2d6e60b61109fb458fdd5fff682b9a7
unboundedimages <majid.muhassan@gmail.com>
2018-12-26 22:53:29 -0500

    removed unneeded code

--- views/js/sqpaymentform.js
+++ views/js/sqpaymentform.js
@@ -1,9 +1,7 @@
-// var applicationId = "sandbox--";
-var applicationId = "-";
+// 'use strict;'
+let appID = document.getElementById('appId').innerHTML;
 
-// Set the location ID
-// var locationId = "";
-var locationId = "";
+// console.log("this is appID: ", appID.reC)
 
 /*
  * function: requestCardNonce
@@ -24,34 +22,17 @@ function requestCardNonce(event) {
 }
 
 // Create and initialize a payment form object
-var paymentForm = new SqPaymentForm({
+let paymentForm = new SqPaymentForm({
 
   // Initialize the payment form elements
   applicationId: applicationId,
-  // applicationId: keys.getArr(),
-  locationId: locationId,
-  // locationId: keys.getArr(),
+  // applicationId: appID[0],
   inputClass: 'sq-input',
 
   // Customize the CSS for SqPaymentForm iframe elements
   inputStyles: [{
       fontSize: '.9em'
   }],
-
-  // Initialize Apple Pay placeholder ID
-  // applePay: {
-  //   elementId: 'sq-apple-pay'
-  // },
-
-  // Initialize Google Pay placeholder ID
-  // googlePay: {
-  //   elementId: 'sq-google-pay'
-  // },
-
-  // Initialize Masterpass placeholder ID
-  // masterpass: {
-  //   elementId: 'sq-masterpass'
-  // },
 
   // Initialize the credit card placeholders
   cardNumber: {
@@ -72,94 +53,6 @@ var paymentForm = new SqPaymentForm({
 
   // SqPaymentForm callback functions
   callbacks: {
-
-    /*
-     * callback function: methodsSupported
-     * `methodsSupported` will be called multiple times, once for each digital
-     * wallet you want to enable.
-     * The `methods` object will contain a single key (applePay, googlePay or
-     * masterpass) with a boolean value indicating whether the digital wallet
-     * is enabled or not. For example, if Google Pay is available, `methods`
-     * will be the object: {googlePay: true}.
-     */
-    // methodsSupported: function (methods) {
-
-    //   var applePayBtn = document.getElementById('sq-apple-pay');
-    //   var googlePayBtn = document.getElementById('sq-google-pay');
-    //   var masterpassBtn = document.getElementById('sq-masterpass');
-
-    //   // Only show the button if Apple Pay on the Web is enabled
-    //   // Otherwise, display the wallet not enabled message.
-    //   if (methods.applePay === true) {
-    //     applePayBtn.style.display = 'inline-block';
-    //   }
-
-    //   // Only show the button if Google Pay is enabled
-    //   if (methods.googlePay === true) {
-    //     googlePayBtn.style.display = 'inline-block';
-    //   }
-
-    //   // Only show the button if Masterpass is enabled
-    //   // Otherwise, display the wallet not enabled message.
-    //   if (methods.masterpass === true) {
-    //     masterpassBtn.style.display = 'inline-block';
-    //   }
-    // },
-
-    /*
-     * callback function: createPaymentRequest
-     * Triggered when: a digital wallet payment button is clicked.
-     */
-    createPaymentRequest: function () {
-      var paymentRequestJson = {
-        requestShippingAddress: true,
-        requestBillingInfo: true,
-        shippingContact: {
-          familyName: "CUSTOMER LAST NAME",
-          givenName: "CUSTOMER FIRST NAME",
-          email: "mycustomer@example.com",
-          country: "USA",
-          region: "CA",
-          city: "San Francisco",
-          addressLines: [
-            "1455 Market St #600"
-          ],
-          postalCode: "94103",
-          phone:"14255551212"
-        },
-        currencyCode: "USD",
-        countryCode: "US",
-        total: {
-          label: "MERCHANT NAME",
-          amount: "85.00",
-          pending: false
-        },
-        lineItems: [
-          {
-            label: "Subtotal",
-            amount: "60.00",
-            pending: false
-          },
-          {
-            label: "Shipping",
-            amount: "19.50",
-            pending: true
-          },
-          {
-            label: "Tax",
-            amount: "5.50",
-            pending: false
-          }
-        ]
-      };
-      return paymentRequestJson;
-    },
-
-    /*
-     * callback function: cardNonceResponseReceived
-     * Triggered when: SqPaymentForm completes a card nonce request
-     */
-    
     cardNonceResponseReceived: function(errors, nonce, cardData, billingContact, shippingContact) {
       function launch_toast2() {
         var x = document.getElementById("toast2")
