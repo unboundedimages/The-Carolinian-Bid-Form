7607ec3159aaf0ef827c2ede50fe14f1c80622ee
unboundedimages <majid.muhassan@gmail.com>
2018-12-26 22:17:52 -0500

    added herf to home route on home icon

--- views/js/media.js
+++ views/js/media.js
@@ -8,17 +8,8 @@ window.onload = function carobanner(){
 	document.getElementById("CaroBanner").innerHTML = caro_banner;
 	let caro_man = '<img src="/img/Caro-Newsman-Mark-223x300.png" class="caroman">';
 	document.getElementById("CaroMan").innerHTML = caro_man;
-	document.getElementById('home').innerHTML = '<div id="line-it"><img src="/img/home-icon-for-website-1.png"></div>';
+	document.getElementById('home').innerHTML = '<div id="line-it"><a href="/"><img src="/img/home-icon-for-website-1.png"></a></div>';
 }
-function redirect()
-{
-    setInterval(function(){
-		window.location='https://carobids.com'
-		// window.onload=redirect;
-	},1000 * 60 * 5);//5 minutes secs
-}
-redirect()
-
 
 function copyRecord(e) { 
 	var x = document.getElementById('invisiline');
