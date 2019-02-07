4469c1f09d4fecd6f4db2e0a17a31df6d62496c4
unboundedimages <majid.muhassan@gmail.com>
2019-02-07 00:45:42 -0500

    added mailer to submit emails from form.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 "use strict";
 const express = require("express");
 const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
 const mysql = require("mysql");
 const env = require("dotenv").load();
 const path = require("path");
@@ -408,6 +409,66 @@ app.get("/fetch-seek-find-locate-name/:id", (req, res) => {
       else console.log(err);
     }
   );
+});
+
+app.get("/contact-us", (req, res) => {
+  res.render("cufh");
+});
+app.post("/send", (req, res) => {
+  const output = `
+    <p>You have a new email from your Carobids.com contact form</p>
+    <h3>Contact Details:</h3>
+    <ul>
+      <li><b>Company</b>: ${req.body.company}</li>
+      <li><b>Name</b>: ${req.body.name}</li>
+      <li><b>Email</b>: ${req.body.email}</li>
+      <li><b>Phone</b>: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>
+      ${req.body.message}
+    </p>
+  `;
+  async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    // let account = await nodemailer.createTestAccount();
+
+    //   // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: process.env.email,
+      // port: 143,
+      // port: 993,
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.emailu, // generated ethereal user
+        pass: process.env.emailp // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: '"CaroBids Help Request" <help@carobids.com>', // sender address
+      to: "rodrossdev@gmail.com", // list of receivers
+      // to: "rodrossdev@gmail.com, adria@caro.news", // list of receivers
+      subject: "CaroBids Form Issue", // Subject line
+      text: "Hello world?", // plain text body
+      html: output // html body
+    };
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail(mailOptions);
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  }
+
+  main().catch(console.error);
+  res.render("cufh", { msg: "Email has been sent" });
 });
 //===============================================================================================
 
