4799e483c48e01c9a6b498eb40d4b8df0ec734a4
unboundedimages <majid.muhassan@gmail.com>
2019-06-07 21:50:24 -0400

    updated includes the ability for client to receive an email when end user processes a payment.  Link to record locator included in email

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//require dependencies
 "use strict";
 const express = require("express");
 const exphbs = require("express-handlebars");
@@ -295,6 +294,55 @@ app.post("/thanks", (req, res, next) => {
           console.log(result.affectedRows + "we won.");
           console.log("this is rec_locator ", pricePaid);
         });
+
+        const output = `
+        <p>You have a new email from your Carobids.com Payments</p>
+            <h3>Contact Details:</h3>
+            <p>              
+              "http://carobids.com/fetch-seek-find-locate/${pricePaid}"
+            </p>
+          `;
+        async function main() {
+          // Generate test SMTP service account from ethereal.email
+          // Only needed if you don't have a real mail account for testing
+          // let account = await nodemailer.createTestAccount();
+
+          //   // create reusable transporter object using the default SMTP transport
+          let transporter = nodemailer.createTransport({
+            host: process.env.email,
+            // port: 143,
+            // port: 993,
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+              user: process.env.emailu, // generated ethereal user
+              pass: process.env.emailp // generated ethereal password
+            },
+            tls: {
+              rejectUnauthorized: false
+            }
+          });
+
+          // setup email data with unicode symbols
+          let mailOptions = {
+            from: '"CaroBids Payments" <payment@carobids.com>', // sender address
+            to:
+              "adria@thejervayagency.com, adria@caro.news, rodrossdev@gmail.com", // list of receivers
+            subject: "CaroBids Processa a Payment", // Subject line
+            text: "Hello world?", // plain text body
+            html: output // html body
+          };
+
+          // send mail with defined transport object
+          let info = await transporter.sendMail(mailOptions);
+
+          console.log("Message sent: %s", info.messageId);
+          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        }
+
+        main().catch(console.error);
+        // res.render("cufh", { msg: "Email has been sent" });
+
         res.render("tycbrs", rec);
       });
     },
